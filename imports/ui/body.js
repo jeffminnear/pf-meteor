6f37d6e85d1501c8660b5a9e77a8400bf56d2f2e
Jeff Minnear <jeffminnear@gmail.com>
2016-04-06 11:31:45 -0400

    Add incomplete scraper function

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,4 +1,30 @@
+'use strict';
+
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+// import { Session } from 'meteor/session';
+
+
+import scrapers from 'pf-scrapers';
 
 import './body.jade';
+
+
+Template.body.events({
+  'submit .search'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+
+    var args = {
+      limit: 5,
+      title: text
+    };
+
+    Meteor.call('getResults', args);
+
+    target.text.value = "";
+  },
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,26 @@
+'use strict';
+
 import { Meteor } from 'meteor/meteor';
+
+import scrapers from 'pf-scrapers';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+/*
+TODO figure out why this method is being called twice
+     once with the correct title, and once with a blank title
+*/
+Meteor.methods({
+  'getResults': function getResults(args) {
+    console.log("##################################################\n" + args.title + "\n##################################################");
+
+    var promises = scrapers;
+
+    Promise.all(promises.map((x) => x(args)))
+      .then((results) => {
+        console.log(results);
+      });
+  }
 });
