9fa90be4fc3ad6adfd7b5bce94b9e3c1ba8358a2
Jeff Minnear <jeffminnear@gmail.com>
2016-04-08 10:23:24 -0400

    Display search results in UI

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,13 +2,28 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-// import { Session } from 'meteor/session';
+import { Session } from 'meteor/session';
 
 
 import scrapers from 'pf-scrapers';
 
 import './body.jade';
+import './result.jade';
 
+// Template.body.onRendered(function() {
+//   const self = this;
+//
+//   Tracker.autorun(() => {
+//     console.log('tracker autorun');
+//     self.scrapeResults = Session.get('scrapeResults');
+//   });
+// });
+
+Template.body.helpers({
+  scrapeResults() {
+    return Session.get('scrapeResults');
+  }
+});
 
 Template.body.events({
   'submit .search'(event) {
@@ -23,7 +38,14 @@ Template.body.events({
       title: text
     };
 
-    Meteor.call('getResults', args);
+    Meteor.call('getResults', args, function(error, result) {
+      if ( result == null ) return;
+
+      Promise.all(result)
+        .then((results) => {
+          Session.set('scrapeResults', results);
+        });
+    });
 
     target.text.value = "";
   },
--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
 
 import scrapers from 'pf-scrapers';
 import _ from 'lodash';
@@ -20,13 +21,17 @@ Meteor.methods({
 
     var promises = scrapers;
 
-    Promise.all(promises.map((x) => x(args)))
-      .then((results) => {
-        results = _.flatten(results);
-        for (var i = 0; i < results.length; i++) {
-          var result = results[i];
-          console.log(result.simplifiedTitle + " is " + result.price + " at " + result.store + "!");
-        }
-      });
+    return new Promise((resolve, reject) => {
+      Promise.all(promises.map((x) => x(args)))
+        .then((results) => {
+          results = _.flatten(results);
+          for (var i = 0; i < results.length; i++) {
+            var result = results[i];
+            console.log(result.simplifiedTitle + " is " + result.price + " at " + result.store + "!");
+          }
+
+          resolve(results);
+        });
+    });
   }
 });
